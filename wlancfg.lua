e73748ae7c437eb7be57a1d6f87a1ffa4d8d96a3
Florian Zahn <fzahn@ccc-mannheim.de>
2017-01-15 15:17:27 +0100

    Fixing minimum PSK length in wlancfg.lua

--- wlancfg.lua
+++ wlancfg.lua
@@ -1,5 +1,5 @@
 -- Tell the chip to connect to this access point
 wifi.setmode(wifi.STATION)
-wifi.sta.config("XXX","XXX")
+wifi.sta.config("XXX","XXXXXXXXXX")
 mqttserver="192.168.23.3"
 mqttbasetopic="/room1/ledstrip"
